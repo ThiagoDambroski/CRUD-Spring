fdf2ab8d8ff0142da98c5480454d7b9cf10c0794
ThiagoDambroski <dambroskic@hotmail.com>
2022-06-02 23:45:00 -0300

    validação e-mail update

--- src/main/java/com/dambroski/primeiro_projeto/dto/ClienteDTO.java
+++ src/main/java/com/dambroski/primeiro_projeto/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.dambroski.primeiro_projeto.domain.Cliente;
+import com.dambroski.primeiro_projeto.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
