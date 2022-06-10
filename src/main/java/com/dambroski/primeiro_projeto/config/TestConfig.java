6d6703500783716ff7784432095201900ad82f7c
ThiagoDambroski <dambroskic@hotmail.com>
2022-06-10 16:31:39 -0300

    Spring Security initial

--- src/main/java/com/dambroski/primeiro_projeto/config/TestConfig.java
+++ src/main/java/com/dambroski/primeiro_projeto/config/TestConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.dambroski.primeiro_projeto.services.DBService;
 import com.dambroski.primeiro_projeto.services.EmailService;
@@ -14,19 +16,23 @@ import com.dambroski.primeiro_projeto.services.MockEmailService;
 @Configuration
 @Profile("test")
 public class TestConfig {
-	
+
 	@Autowired
 	private DBService dbService;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
-	
+
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
 
+	@Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
 }
