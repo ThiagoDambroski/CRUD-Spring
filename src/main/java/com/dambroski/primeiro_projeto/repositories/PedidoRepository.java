45fc2865eec2b26344b97e42e8d6b337f72616f0
ThiagoDambroski <dambroskic@hotmail.com>
2022-06-27 12:09:20 -0300

    cliente so recupera seus pedidos

--- src/main/java/com/dambroski/primeiro_projeto/repositories/PedidoRepository.java
+++ src/main/java/com/dambroski/primeiro_projeto/repositories/PedidoRepository.java
@@ -1,12 +1,19 @@
 package com.dambroski.primeiro_projeto.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dambroski.primeiro_projeto.domain.Cliente;
 import com.dambroski.primeiro_projeto.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente,Pageable pageResquest);
 	
 
 }
--- src/main/java/com/dambroski/primeiro_projeto/services/PedidoService.java
+++ src/main/java/com/dambroski/primeiro_projeto/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dambroski.primeiro_projeto.domain.Cliente;
 import com.dambroski.primeiro_projeto.domain.ItemPedido;
 import com.dambroski.primeiro_projeto.domain.PagamentoComBoleto;
 import com.dambroski.primeiro_projeto.domain.Pedido;
@@ -14,6 +18,8 @@ import com.dambroski.primeiro_projeto.domain.enums.EstadoPagamento;
 import com.dambroski.primeiro_projeto.repositories.ItemPedidoRepository;
 import com.dambroski.primeiro_projeto.repositories.PagamentoRepository;
 import com.dambroski.primeiro_projeto.repositories.PedidoRepository;
+import com.dambroski.primeiro_projeto.security.UserSS;
+import com.dambroski.primeiro_projeto.services.exceptions.AuthorizationExeception;
 import com.dambroski.primeiro_projeto.services.exceptions.ObjectNotFoundExeception;
 
 @Service
@@ -69,6 +75,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy,String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationExeception("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
